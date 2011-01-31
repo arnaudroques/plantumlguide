84d56ac5b9d6c7ac91784d432e568192c8cf6385
Arnaud Roques <plantuml@gmail.com>
2011-01-31 22:20:09 +0100

    Add files

---
+++ build1.bat
@@ -0,0 +1,9 @@
+java -jar \plantuml.jar -charset UTF-8 -verbose *.tex
+"C:\Program Files\MiKTeX 2.8\miktex\bin\xelatex.exe" -interaction=nonstopmode --src-specials "PlantUML Language Reference Guide.tex"
+"C:\Program Files\MiKTeX 2.8\miktex\bin\xelatex.exe" -interaction=nonstopmode --src-specials "PlantUML Language Reference Guide.tex"
+"C:\Program Files\MiKTeX 2.8\miktex\bin\xelatex.exe" -interaction=nonstopmode --src-specials "PlantUML Language Reference Guide.tex"
+del *.png
+del *.aux
+del *.toc
+del *.out
+pause 
